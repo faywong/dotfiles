cb5db1043ee735d8a4aeb3a747c7b93d2f01210e
faywong <philip584521@gmail.com>
2023-03-27 10:33:37 +0800

    Create plugins.lua

---
+++ nvim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,25 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Plugins can have dependencies on other plugins
+  use {
+    'haorenW1025/completion-nvim',
+    opt = true,
+    requires = {{'hrsh7th/vim-vsnip', opt = true}, {'hrsh7th/vim-vsnip-integ', opt = true}}
+  }
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use 'Olical/conjure'
+  use 'bakpakin/janet.vim'
+
+end)
