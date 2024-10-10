74c808e75e0bb56661df3ab08870ea53c531b9a3
faywong <philip584521@gmail.com>
2024-10-10 22:59:09 +0800

    add github520 script to update hosts to access raw.github.com

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+echo "updating hosts file avoid github 404..."
+sed -i "/# GitHub520 Host Start/Q" /etc/hosts && curl https://raw.hellogithub.com/hosts >> /etc/hosts
 cp ./vim/.vimrc ~/
 cp ./git/.gitconfig ~/
