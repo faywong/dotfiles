adeac83cb60068447595277900a0b8b10c459725
faywong <philip584521@gmail.com>
2024-11-15 14:00:27 +0800

    add npm registry config for coc.nvim

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,26 @@ echo "installing dev toolkit"
 sudo apt install -y clangd ripgrep nodejs python3
 echo "updating hosts file avoid github 404..."
 sed -i "/# GitHub520 Host Start/Q" /etc/hosts && curl https://raw.hellogithub.com/hosts >> /etc/hosts
+# 定义 .npmrc 文件路径
+NPMRC="$HOME/.npmrc"
+
+# 定义要检查的键和值
+KEY="coc.nvim:registry"
+VALUE="coc.nvim:registry=http://mirrors.cloud.tencent.com/npm/"
+
+# 如果 .npmrc 文件不存在，则创建它
+if [ ! -f "$NPMRC" ]; then
+  touch "$NPMRC"
+  echo "已创建 $NPMRC 文件。"
+fi
+
+# 检查 .npmrc 文件中是否已经存在指定的键
+if ! grep -q "^$KEY=" "$NPMRC"; then
+  # 如果不存在，则追加新的配置
+  echo "$VALUE" >> "$NPMRC"
+  echo "已将 '$VALUE' 追加到 $NPMRC。"
+else
+  echo "'$KEY' 已存在于 $NPMRC 中，无需追加。"
+fi
 cp ./vim/.vimrc ~/
 cp ./git/.gitconfig ~/
