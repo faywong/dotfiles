601d9426bcca471e6f96f1a1beb6f2ec96ad26a4
faywong <philip584521@gmail.com>
2025-08-24 21:27:57 +0800

    Create compile.sh

---
+++ vim/compile.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# compile the latest vim with huge feature included(lua, python3, perl, xclip...)
+
+./configure --with-vim-name=vim --with-features=huge --enable-gui=auto --with-x --enable-rubyinterp --with-ruby-command=/usr/bin/ruby --enable-luainterp  --enable-perlinterp --enable-python3interp --with-python3-config-dir=$(bash /usr/bin/python3.13-config --configdir) --enable-fontset --enable-gtk2-check --enable-gnome-check --enable-multibyte --with-compiledby="YourName"  --enable-fail-if-missing
