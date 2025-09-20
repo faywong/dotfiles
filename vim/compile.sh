3f554345612e0159a0fe528bc46b84e9a5acf274
faywong <philip584521@gmail.com>
2025-09-20 08:20:43 +0800

    Update compile.sh

--- vim/compile.sh
+++ vim/compile.sh
@@ -3,4 +3,4 @@
 
 sudo apt install ruby-dev lua5.2 liblua5.2-dev python3-dev perl libperl-dev
 
-./configure --with-vim-name=vim --with-features=huge --enable-gui=auto --with-x --enable-rubyinterp --with-ruby-command=/usr/bin/ruby --enable-luainterp  --enable-perlinterp --enable-python3interp --with-python3-config-dir=$(bash /usr/bin/python3.13-config --configdir) --enable-fontset --enable-gtk2-check --enable-gnome-check --enable-multibyte --with-compiledby="faywong"  --enable-fail-if-missing
+./configure --with-vim-name=vim --with-features=huge --enable-gui=auto --with-x --disable-gui --enable-rubyinterp --with-ruby-command=/usr/bin/ruby --enable-luainterp  --enable-perlinterp --enable-python3interp --with-python3-config-dir=$(bash /usr/bin/python3-config --configdir) --enable-fontset --enable-gtk2-check --enable-gnome-check --enable-multibyte --with-compiledby="faywong"  --enable-fail-if-missing
