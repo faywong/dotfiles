4dadab026b87e9c85cb4a71c9da0f054cb850f40
faywong <philip584521@gmail.com>
2026-03-29 09:43:20 +0800

    Update compile.sh

--- vim/compile.sh
+++ vim/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # compile the latest vim with huge feature included(lua, python3, perl, clipboard...)
 
-sudo apt install ruby-dev lua5.2 liblua5.2-dev python3-dev perl libperl-dev
+sudo apt install ruby-dev lua5.2 liblua5.2-dev python3-dev perl libperl-dev racket
 
-./configure --with-vim-name=vim --with-features=huge --enable-gui=auto --with-x --disable-gui --enable-rubyinterp --with-ruby-command=/usr/bin/ruby --enable-luainterp  --enable-perlinterp --enable-python3interp --with-python3-config-dir=$(bash /usr/bin/python3-config --configdir) --enable-fontset --enable-gtk2-check --enable-gnome-check --enable-multibyte --with-compiledby="faywong"  --enable-fail-if-missing
+./configure --with-vim-name=vim --with-features=huge --enable-gui=auto --with-x --enable-mzschemeinterp --enable-cscope --disable-gui --enable-rubyinterp --with-ruby-command=/usr/bin/ruby --enable-luainterp  --enable-perlinterp --enable-python3interp --with-python3-config-dir=$(bash /usr/bin/python3-config --configdir) --enable-fontset --enable-gtk2-check --enable-gnome-check --enable-multibyte --with-compiledby="faywong"  --enable-fail-if-missing
